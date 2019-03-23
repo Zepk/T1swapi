aa9d98414766821723f9be4c1b68bd8f76bbb2bb
zepk <zepktausao@gmail.com>
2019-03-23 18:03:17 -0300

    index page

--- swapihandler/views.py
+++ swapihandler/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+import requests
+import json
 
 
 def index(request):
-    return HttpResponse("Hello!")
+    r = requests.get('https://swapi.co/api/films')
+    datos = r.json()
+    template = loader.get_template('swapihandler/index.html')
+    context = {
+        'datos': datos,
+    }
+    return HttpResponse(template.render(context, request))
